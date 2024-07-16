3975792a83a83a6ca2651e5607f7aa8614d20a25
gr0grig <mr.grogrig@gmail.com>
2024-07-16 05:59:49 +0000

    remove copies 2

--- components/stores.ts
+++
@@ -1,8 +0,0 @@
-import TestPlugin from 'main';
-import { get, writable } from "svelte/store";
-import { BaseScanner } from "../base_scanner";
-
-export const plugin = writable<TestPlugin>();
-export const getPlugin = () => get(plugin);
-export const data = writable<BaseScanner>();
-export const active_id = writable<string>();
